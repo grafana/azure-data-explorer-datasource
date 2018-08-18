1d285654234a02b5ed12b38c4f844cd90e3c5d37
Daniel Lee <dan.limerick@gmail.com>
2018-08-18 16:29:22 +0200

    adds cluster url field to config
    Depends on this PR in Grafana to work:
    https://github.com/grafana/grafana/pull/12974

--- src/config_ctrl.ts
+++ src/config_ctrl.ts
@@ -1,3 +1,14 @@
 export class KustoDBConfigCtrl {
   static templateUrl = 'partials/config.html';
+
+  current: any;
+  suggestUrl: string;
+
+  /** @ngInject */
+  constructor($scope) {
+    this.suggestUrl = 'https://yourcluster.kusto.windows.net';
+    $scope.getSuggestUrls = () => {
+      return [this.suggestUrl];
+    };
+  }
 }
