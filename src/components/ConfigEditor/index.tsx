63224133a73231f5e1877c37639f5a6f396953cf
joshhunt <josh@trtr.co>
2021-03-17 15:03:51 +0000

    Fix: show errors from refreshing schema

--- src/components/ConfigEditor/index.tsx
+++ src/components/ConfigEditor/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
+import { FetchResponse } from '@grafana/runtime';
 import ConfigHelp from 'components/ConfigEditor/ConfigHelp';
 import { AdxDataSourceOptions, AdxDataSourceSecureOptions } from 'types';
 import ConnectionConfig from './ConnectionConfig';
@@ -7,14 +8,32 @@ import DatabaseConfig from './DatabaseConfig';
 import QueryConfig from './QueryConfig';
 import { refreshSchema, Schema } from './refreshSchema';
 import TrackingConfig from './TrackingConfig';
+import { alertError } from '@grafana/data/types/appEvents';
+import { Alert } from '@grafana/ui';
 
 interface ConfigEditorProps
   extends DataSourcePluginOptionsEditorProps<AdxDataSourceOptions, AdxDataSourceSecureOptions> {}
 
+type FetchErrorResponse = FetchResponse<{
+  error?: string;
+  message?: string;
+  response?: string;
+}>;
+
 const ConfigEditor: React.FC<ConfigEditorProps> = props => {
   const { options, onOptionsChange } = props;
   const [schema, setSchema] = useState<Schema>({ databases: [], schemaMappingOptions: [] });
+  const [schemaError, setSchemaError] = useState<FetchErrorResponse['data']>();
   const { jsonData } = options;
+
+  const updateSchema = (url: string) => {
+    refreshSchema(url)
+      .then(data => {
+        setSchema(data);
+        setSchemaError(undefined);
+      })
+      .catch((err: FetchErrorResponse) => setSchemaError(err.data));
+  };
 
   const updateJsonData = useCallback(
     <T extends keyof AdxDataSourceOptions>(fieldName: T, value: AdxDataSourceOptions[T]) => {
@@ -30,9 +49,7 @@ const ConfigEditor: React.FC<ConfigEditorProps> = props => {
   );
 
   useEffect(() => {
-    if (options.id) {
-      refreshSchema(options.url).then(data => setSchema(data));
-    }
+    options.id && updateSchema(options.url);
   }, [options.id, options.url]);
 
   useEffect(() => {
@@ -42,7 +59,7 @@ const ConfigEditor: React.FC<ConfigEditorProps> = props => {
   }, [schema?.databases, jsonData.defaultDatabase, updateJsonData]);
 
   const handleRefreshClick = useCallback(() => {
-    refreshSchema(options.url).then(data => setSchema(data));
+    updateSchema(options.url);
   }, [options.url]);
 
   const handleClearClientSecret = useCallback(() => {
@@ -81,6 +98,12 @@ const ConfigEditor: React.FC<ConfigEditorProps> = props => {
       />
 
       <TrackingConfig options={options} onOptionsChange={onOptionsChange} updateJsonData={updateJsonData} />
+
+      {schemaError && (
+        <Alert severity="error" title="Error updating Azure Data Explorer schema">
+          {schemaError.message}
+        </Alert>
+      )}
     </>
   );
 };
