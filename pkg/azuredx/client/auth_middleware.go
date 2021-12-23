dbe8a83fc7d74214d700b25b1ce0c8c73afe2b14
Pascal S. de Kloe <pascal@quies.net>
2021-12-23 18:14:41 +0100

    FIX: Prevent overwrite from (on-behalf-of) user token.

--- pkg/azuredx/client/auth_middleware.go
+++ pkg/azuredx/client/auth_middleware.go
@@ -20,12 +20,14 @@ type TokenProvider interface {
 func AuthMiddleware(tokenProvider TokenProvider) httpclient.Middleware {
 	return httpclient.NamedMiddlewareFunc(authenticationMiddlewareName, func(opts httpclient.Options, next http.RoundTripper) http.RoundTripper {
 		return httpclient.RoundTripperFunc(func(req *http.Request) (*http.Response, error) {
-			token, err := tokenProvider.GetAccessToken(req.Context())
-			if err != nil {
-				logError("failed to retrieve azure access token", "error", err)
-				return next.RoundTrip(req)
+			if req.Header.Get("Authorization") == "" {
+				token, err := tokenProvider.GetAccessToken(req.Context())
+				if err != nil {
+					logError("failed to retrieve azure access token", "error", err)
+					return next.RoundTrip(req)
+				}
+				req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
 			}
-			req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
 
 			res, err := next.RoundTrip(req)
 			return res, err
