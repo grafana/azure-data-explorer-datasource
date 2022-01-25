3aa148c25cf90e6ff21284e50e526bdaae324876
Shirley Leu <4163034+fridgepoet@users.noreply.github.com>
2022-01-25 16:19:08 +0100

    Add aria-labels to connection config

--- src/components/ConfigEditor/ConnectionConfig.tsx
+++ src/components/ConfigEditor/ConnectionConfig.tsx
@@ -62,6 +62,7 @@ const ConnectionConfig: React.FC<ConnectionConfigProps> = ({
     <FieldSet label="Connection Details">
       <InlineField label="Azure cloud" labelWidth={26} tooltip="Select an Azure Cloud." required>
         <Select
+          aria-label='Azure cloud'
           options={azureClouds}
           value={azureClouds.find((v) => v.value === jsonData.azureCloud)}
           onChange={(change: SelectableValue<AzureCloudType>) =>
@@ -137,6 +138,7 @@ const ConnectionConfig: React.FC<ConnectionConfigProps> = ({
 
       <SecretFormField
         label="Client secret"
+        aria-label="Client secret"
         value={secureJsonData?.clientSecret || undefined}
         labelWidth={13}
         inputWidth={30}
