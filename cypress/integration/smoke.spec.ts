0ea3d6a6b2ce2ba13524852dad374d6aab936f21
Shirley Leu <4163034+fridgepoet@users.noreply.github.com>
2022-01-31 18:23:49 +0100

    Target added datasource in dashboard json

--- cypress/integration/smoke.spec.ts
+++ cypress/integration/smoke.spec.ts
@@ -48,6 +48,25 @@ e2e.scenario({
   describeName: 'Import dashboard',
   itName: 'adds JSON',
   scenario: () => {
-    e2e.flows.importDashboard(TEST_DASHBOARD);
+    e2e()
+      .readProvisions(['datasources/adx.yaml'])
+      .then((ADXProvisions: ADXProvision[]) => {
+        const datasource = ADXProvisions[0].datasources[1]; // Second entry in our common provisioning file
+
+        e2e.flows.addDataSource({
+          name: 'e2e-azure-data-explorer-datasource',
+          type: 'Azure Data Explorer Datasource',
+          form: () => {
+            e2eSelectors.configEditor.azureCloud.input().type('Azure');
+            e2eSelectors.configEditor.clusterURL.input().click({ force: true }).type(datasource.jsonData.clusterUrl);
+            e2eSelectors.configEditor.tenantID.input().type(datasource.jsonData.tenantId);
+            e2eSelectors.configEditor.clientID.input().type(datasource.jsonData.clientId);
+            e2eSelectors.configEditor.clientSecret.input().type(datasource.secureJsonData.clientSecret);
+          },
+          expectedAlertMessage: 'Success',
+        });
+
+        e2e.flows.importDashboard(TEST_DASHBOARD);
+      });
   },
 });
