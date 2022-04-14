2076be1224c340329c2a3a3fa1cdc70b6d78fd0d
Sergey Kostrukov <sergey@kostrukov.com>
2022-04-15 04:49:34 +0700

    Optimize implementation of getAzureScopes

--- pkg/azuredx/azureauth/scopes.go
+++ pkg/azuredx/azureauth/scopes.go
@@ -9,10 +9,10 @@ import (
 )
 
 var (
-	azureDataExplorerScopes = map[string][]string{
-		azsettings.AzurePublic:       {"https://kusto.kusto.windows.net/.default"},
-		azsettings.AzureChina:        {"https://kusto.kusto.chinacloudapi.cn/.default"},
-		azsettings.AzureUSGovernment: {"{clusterUrl}/.default"},
+	azureDataExplorerScopes = map[string]string{
+		azsettings.AzurePublic:       "https://kusto.kusto.windows.net/.default",
+		azsettings.AzureChina:        "https://kusto.kusto.chinacloudapi.cn/.default",
+		azsettings.AzureUSGovernment: "{clusterUrl}/.default",
 	}
 )
 
@@ -20,16 +20,13 @@ func getAzureScopes(credentials *azcredentials.AzureClientSecretCredentials, clu
 	azureCloud := credentials.AzureCloud
 
 	// Get scopes for the given cloud
-	if scopesTmpl, ok := azureDataExplorerScopes[azureCloud]; !ok {
+	if scopeTmpl, ok := azureDataExplorerScopes[azureCloud]; !ok {
 		err := fmt.Errorf("the Azure cloud '%s' not supported by Azure Data Explorer datasource", azureCloud)
 		return nil, err
 	} else {
 		// TODO: #356 Generalize generation of scopes for all clouds
 		clusterUrl = strings.TrimSuffix(clusterUrl, "/")
-		scopes := make([]string, len(scopesTmpl))
-		for i, scopeTmpl := range scopesTmpl {
-			scopes[i] = strings.Replace(scopeTmpl, "{clusterUrl}", clusterUrl, 1)
-		}
+		scopes := []string{strings.Replace(scopeTmpl, "{clusterUrl}", clusterUrl, 1)}
 		return scopes, nil
 	}
 }
