0883decc8f1711f59764958948daaa95583ac12f
Andres Martinez Gotor <andres.mgotor@gmail.com>
2022-04-28 17:40:31 +0200

    review

--- src/common/responseHelpers.test.ts
+++ src/common/responseHelpers.test.ts
@@ -1,23 +1,16 @@
-import { DataFrame, FieldType } from '@grafana/data';
+import { FieldType, toDataFrame } from '@grafana/data';
 import { firstStringFieldToMetricFindValue } from './responseHelpers';
 
 describe('firstStringFieldToMetricFindValue', () => {
   it('should find a string field value', () => {
-    const frame: DataFrame = {
+    const frame = toDataFrame({
       fields: [
         {
           type: FieldType.string,
-          name: '',
-          config: {},
-          values: {
-            length: 1,
-            get: () => 'foo',
-            toArray: () => [],
-          },
+          values: ['foo'],
         },
       ],
-      length: 1,
-    };
+    });
     expect(firstStringFieldToMetricFindValue(frame)).toEqual([{ text: 'foo' }]);
   });
 });
