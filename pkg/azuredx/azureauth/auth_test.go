9d439a2aa0fe347f039b23f64909fa5dd1725929
yaellec <yaelle.chaudy@grafana.com>
2022-05-30 11:26:50 +0200

    removed todo

--- pkg/azuredx/azureauth/auth_test.go
+++ pkg/azuredx/azureauth/auth_test.go
@@ -86,7 +86,6 @@ func TestOnBehalfOf(t *testing.T) {
 	}
 }
 
-// TODO test feature flag disabled too -> should have same result
 func TestOnBehalfOfDisabled(t *testing.T) {
 	// compose request
 	var req backend.QueryDataRequest
