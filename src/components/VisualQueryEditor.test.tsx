260e4b0dbf614136c090ad1d18af81b0fa3353c9
Andres Martinez Gotor <andres.mgotor@gmail.com>
2022-06-01 13:03:59 +0200

    Add support for dynamic objects in Aggregate and Group by fields (#390)

---
+++ src/components/VisualQueryEditor.test.tsx
@@ -0,0 +1,59 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import React from 'react';
+import { VisualQueryEditor } from './VisualQueryEditor';
+import { mockDatasource, mockQuery } from './__fixtures__/Datasource';
+
+jest.mock('@grafana/runtime', () => {
+  const original = jest.requireActual('@grafana/runtime');
+  return {
+    ...original,
+    getTemplateSrv: () => ({
+      getVariables: () => [],
+      replace: (s: string) => s,
+    }),
+  };
+});
+
+const defaultProps = {
+  database: '',
+  query: mockQuery,
+  onChangeQuery: jest.fn(),
+  datasource: mockDatasource,
+  templateVariableOptions: {},
+};
+
+const schema = {
+  Databases: {
+    foo: {
+      Name: 'foo',
+      Tables: {
+        bar: {
+          Name: 'bar',
+          OrderedColumns: [
+            {
+              Name: 'foobar',
+              CslType: 'string',
+            },
+          ],
+        },
+      },
+      ExternalTables: {},
+      Functions: {},
+      MaterializedViews: {},
+    },
+  },
+};
+
+describe('VisualQueryEditor', () => {
+  it('should render the VisualQueryEditor', async () => {
+    render(<VisualQueryEditor {...defaultProps} />);
+    await waitFor(() => screen.getByText('Table schema loaded successfully but without any columns'));
+  });
+
+  it('should render the VisualQueryEditor with a schema', async () => {
+    const datasource = mockDatasource;
+    datasource.getSchema = jest.fn().mockResolvedValue(schema);
+    render(<VisualQueryEditor {...defaultProps} datasource={datasource} database="foo" schema={schema} />);
+    await waitFor(() => screen.getByText('bar'));
+  });
+});
