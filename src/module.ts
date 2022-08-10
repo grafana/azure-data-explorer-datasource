fbcc954c2af740e3f5adfd147f6919a0672546e4
Andres Martinez <andres.martinez@grafana.com>
2022-08-10 17:31:46 +0200

    Add query editor help

--- src/module.ts
+++ src/module.ts
@@ -4,9 +4,11 @@ import ConfigEditor from 'components/ConfigEditor';
 import { AdxDataSource } from './datasource';
 import { QueryEditor } from './components/QueryEditor';
 import { AdxDataSourceOptions, AdxDataSourceSecureOptions, KustoQuery } from './types';
+import EditorHelp from 'components/QueryEditor/EditorHelp';
 
 export const plugin = new DataSourcePlugin<AdxDataSource, KustoQuery, AdxDataSourceOptions, AdxDataSourceSecureOptions>(
   AdxDataSource
 )
   .setConfigEditor(ConfigEditor)
+  .setQueryEditorHelp(EditorHelp)
   .setQueryEditor(QueryEditor);
