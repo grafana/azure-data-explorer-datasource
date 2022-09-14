c35dc932fdf35bd746f014c88228439f04dcb198
Andres Martinez <andres.martinez@grafana.com>
2022-09-14 16:54:17 +0200

    Use Prismjs highlight

--- src/components/QueryEditor/VisualQueryEditor/KQLPreview.tsx
+++ src/components/QueryEditor/VisualQueryEditor/KQLPreview.tsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { EditorField, EditorFieldGroup, EditorRow } from '@grafana/experimental';
 import { Button, useStyles2 } from '@grafana/ui';
 import { css } from '@emotion/css';
 import { GrafanaTheme2 } from '@grafana/data';
+import Prism from 'prismjs';
+import 'prismjs/components/prism-kusto';
+import 'prismjs/themes/prism-tomorrow.min.css';
 
 interface KQLPreviewProps {
   query: string;
@@ -11,6 +14,10 @@ interface KQLPreviewProps {
 const KQLPreview: React.FC<KQLPreviewProps> = ({ query }) => {
   const styles = useStyles2(getStyles);
   const [hidden, setHidden] = useState(true);
+
+  useEffect(() => {
+    Prism.highlightAll();
+  }, [query]);
 
   return (
     <EditorRow>
@@ -21,7 +28,9 @@ const KQLPreview: React.FC<KQLPreviewProps> = ({ query }) => {
               show
             </Button>
             <div className={styles.codeBlock} hidden={hidden}>
-              <pre className={styles.code}>{query}</pre>
+              <pre className={styles.code}>
+                <code className="language-kusto">{query}</code>
+              </pre>
             </div>
             <Button hidden={hidden} variant="secondary" onClick={() => setHidden(true)} size="sm">
               hide
