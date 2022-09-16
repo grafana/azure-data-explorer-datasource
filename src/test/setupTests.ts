e9528e4dded0fd9601a2851927e46d614823cad7
Andres Martinez <andres.martinez@grafana.com>
2022-09-16 13:19:52 +0200

    debug: sync with grafana versions

--- src/test/setupTests.ts
+++ src/test/setupTests.ts
@@ -16,5 +16,5 @@ Object.defineProperty(window, 'matchMedia', {
 
 // react-select-event requires jest 28 but @grafana/toolkit does not support it yet
 // so we need to redefine these to bypass the syntax error
-Object.defineProperty(window, 'setImmediate', window.setTimeout);
-Object.defineProperty(window, 'clearImmediate', window.clearTimeout);
+// Object.defineProperty(window, 'setImmediate', window.setTimeout);
+// Object.defineProperty(window, 'clearImmediate', window.clearTimeout);
