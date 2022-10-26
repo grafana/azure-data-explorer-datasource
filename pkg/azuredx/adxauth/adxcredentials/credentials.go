fe5c5159ee09908d6de02663fdb2197c43fc5c16
Sergey Kostrukov <sekost@microsoft.com>
2022-10-25 18:17:28 -0700

    Cloud from the SDK

--- pkg/azuredx/adxauth/adxcredentials/credentials.go
+++ pkg/azuredx/adxauth/adxcredentials/credentials.go
@@ -1,8 +1,6 @@
 package adxcredentials
 
 import (
-	"fmt"
-
 	"github.com/grafana/grafana-azure-sdk-go/azcredentials"
 	"github.com/grafana/grafana-azure-sdk-go/azsettings"
 )
@@ -11,30 +9,6 @@ func GetDefaultCredentials(settings *azsettings.AzureSettings) azcredentials.Azu
 	if settings.ManagedIdentityEnabled {
 		return &azcredentials.AzureManagedIdentityCredentials{}
 	} else {
-		azureCloud := getDefaultAzureCloud(settings)
-		return &azcredentials.AzureClientSecretCredentials{AzureCloud: azureCloud}
+		return &azcredentials.AzureClientSecretCredentials{AzureCloud: settings.GetDefaultCloud()}
 	}
-}
-
-func GetAzureCloud(settings *azsettings.AzureSettings, credentials azcredentials.AzureCredentials) (string, error) {
-	switch c := credentials.(type) {
-	case *azcredentials.AzureManagedIdentityCredentials:
-		// In case of managed identity, the cloud is always same as where Grafana is hosted
-		return getDefaultAzureCloud(settings), nil
-	case *azcredentials.AzureClientSecretCredentials:
-		return c.AzureCloud, nil
-	case *azcredentials.AzureClientSecretOboCredentials:
-		return c.ClientSecretCredentials.AzureCloud, nil
-	default:
-		err := fmt.Errorf("the Azure credentials of type '%s' not supported by Azure Data Explorer datasource", c.AzureAuthType())
-		return "", err
-	}
-}
-
-func getDefaultAzureCloud(settings *azsettings.AzureSettings) string {
-	cloudName := settings.Cloud
-	if cloudName == "" {
-		return azsettings.AzurePublic
-	}
-	return cloudName
 }
