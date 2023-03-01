872f738ca162f2bafb87b7ce0c6385ed6f473aa5
alyssabull <alyssabull@gmail.com>
2023-03-01 14:51:06 -0700

    Update conditional to preserve adx time series format in raw mode

--- src/components/QueryEditor/QueryHeader.tsx
+++ src/components/QueryEditor/QueryHeader.tsx
@@ -71,8 +71,8 @@ export const QueryHeader = (props: QueryEditorHeaderProps) => {
     if (!query.resultFormat) {
       onChange({ ...query, resultFormat: 'table' });
     }
-    if (query.resultFormat === adxTimeFormat.value && !formats.includes(adxTimeFormat)) {
-      // Fallback to Time Series since time_series_adx_series is not available
+    if (query.resultFormat === adxTimeFormat.value && !rawMode) {
+      // Fallback to Time Series since time_series_adx_series is not available when not in rawMode
       onChange({ ...query, resultFormat: 'time_series' });
     }
   }, [query, formats, onChange]);
