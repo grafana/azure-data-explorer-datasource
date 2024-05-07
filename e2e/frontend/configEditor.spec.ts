cc6689a8fe240983eb902ce7ea1f8eab19313c9b
Andreas Christou <andreas.christou@grafana.com>
2024-05-07 19:16:01 +0100

    Add config editor e2e test

---
+++ e2e/frontend/configEditor.spec.ts
@@ -0,0 +1,37 @@
+import { expect, test } from '@grafana/plugin-e2e';
+import { selectors } from '../../src/test/selectors';
+import { AdxDataSourceOptions, AdxDataSourceSecureOptions } from '../../src/types';
+
+test.describe('Create Azure Data Explorer datasource', () => {
+  test('fills out datasource connection configuration', async ({
+    createDataSourceConfigPage,
+    page,
+    readProvisionedDataSource,
+  }) => {
+    const configPage = await createDataSourceConfigPage({
+      type: 'grafana-azure-data-explorer-datasource',
+    });
+    const datasource = await readProvisionedDataSource<AdxDataSourceOptions, AdxDataSourceSecureOptions>({
+      fileName: 'adx.yaml',
+    });
+    // This line is needed if feature flags for additional auth are enabled
+    await page.getByTestId(selectors.components.configEditor.authType.input).getByRole('combobox').click();
+    await page.keyboard.insertText('App Registration');
+    await page.keyboard.press('Enter');
+    const credentials = datasource.jsonData.azureCredentials;
+    if (credentials && credentials.authType === 'clientsecret') {
+      await page.getByTestId(selectors.components.configEditor.azureCloud.input).getByRole('combobox').click();
+      await page.keyboard.insertText('Azure');
+      await page.keyboard.press('Enter');
+      await page.getByTestId(selectors.components.configEditor.tenantID.input).click();
+      await page.keyboard.insertText(credentials.tenantId || '');
+      await page.getByTestId(selectors.components.configEditor.clientID.input).click();
+      await page.keyboard.insertText(credentials.clientId || '');
+      await page.getByTestId(selectors.components.configEditor.clientSecret.input).click();
+      await page.keyboard.insertText((credentials.clientSecret as string) || '');
+      await page.getByTestId(selectors.components.configEditor.clusterURL.input).click();
+      await page.keyboard.insertText(datasource.jsonData.clusterUrl);
+    }
+    await expect(configPage.saveAndTest()).toBeOK();
+  });
+});
