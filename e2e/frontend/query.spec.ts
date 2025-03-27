66565be565b80c07052c24889c2ffcc2cc904c0d
Andreas Christou <andreas.christou@grafana.com>
2025-03-27 14:32:41 +0000

    Update plugin (#1229)
    * Update plugin
    
    * Remove deprecated prop
    
    * Add mock IntersectionObserver and bump plugin-e2e
    
    * Yarn
    
    * Create-plugin bump
    
    * Update E2E tests

--- e2e/frontend/query.spec.ts
+++ e2e/frontend/query.spec.ts
@@ -10,10 +10,6 @@ test.describe('Azure Data Explorer queries', () => {
       name: 'Azure Data Explorer',
     });
     const panel = await dashboardPage.addPanel();
-    await dashboardPage.timeRange.set({
-      from: '2019-10-05 19:00:00',
-      to: '2019-10-10 19:00:00',
-    });
     await panel.datasource.set(datasource.name);
     await panel.setVisualization('Table');
 
@@ -28,7 +24,7 @@ test.describe('Azure Data Explorer queries', () => {
       // data-testid was not passed to the select component prior to 11.1.0
       await panel.getQueryEditorRow('A').getByText('Cluster').click({ force: true });
     }
-    await page.getByLabel('Select options menu').getByText('grafanaadxdev').click({ force: true });
+    await page.getByLabel('Select options menu').getByText('adxtestclustere2e').click({ force: true });
     if (versionValue) {
       await panel
         .getQueryEditorRow('A')
@@ -38,19 +34,19 @@ test.describe('Azure Data Explorer queries', () => {
       // data-testid was not passed to the select component prior to 11.1.0
       await panel.getQueryEditorRow('A').getByText('Database').click({ force: true });
     }
-    await page.getByLabel('Select options menu').getByText('PerfTest').click({ force: true });
+    await page.getByLabel('Select options menu').getByText('adx-test-db').click({ force: true });
     await page.getByText('KQL').click({ force: true });
     await page.waitForTimeout(10000);
     await page.getByTestId(selectors.components.queryEditor.codeEditor.container).click();
     await page.keyboard.press('Control+A');
-    await page.keyboard.insertText('PerfTest | where $__');
+    await page.keyboard.insertText('adxe2etest | where $__');
     await page.waitForTimeout(10000);
     await page.keyboard.press('Control+Space');
     await expect(page.getByText('$__timeFilter')).toBeVisible();
-    await page.keyboard.insertText('timeFilter(_Timestamp_) | order by _Timestamp_ asc');
+    await page.keyboard.insertText('timeFilter(timestamp) | order by timestamp asc');
     await page.getByTestId(selectors.components.queryEditor.runQuery.button).click();
     await page.waitForTimeout(6000);
-    expect(panel.panel.fieldNames).toContainText(['_val1_', '_val2_']);
+    expect(panel.panel.fieldNames).toContainText(['timestamp', 'name', 'index', 'jsonData', 'array', 'static']);
   });
 
   test('Create a KQL query and run using Shift+Enter', async ({
@@ -64,10 +60,6 @@ test.describe('Azure Data Explorer queries', () => {
       name: 'Azure Data Explorer',
     });
     const panel = await dashboardPage.addPanel();
-    await dashboardPage.timeRange.set({
-      from: '2019-10-05 19:00:00',
-      to: '2019-10-10 19:00:00',
-    });
     await panel.datasource.set(datasource.name);
     await panel.setVisualization('Table');
 
@@ -82,7 +74,7 @@ test.describe('Azure Data Explorer queries', () => {
       // data-testid was not passed to the select component prior to 11.1.0
       await panel.getQueryEditorRow('A').getByText('Cluster').click({ force: true });
     }
-    await page.getByLabel('Select options menu').getByText('grafanaadxdev').click({ force: true });
+    await page.getByLabel('Select options menu').getByText('adxtestclustere2e').click({ force: true });
     if (versionValue) {
       await panel
         .getQueryEditorRow('A')
@@ -92,19 +84,19 @@ test.describe('Azure Data Explorer queries', () => {
       // data-testid was not passed to the select component prior to 11.1.0
       await panel.getQueryEditorRow('A').getByText('Database').click({ force: true });
     }
-    await page.getByLabel('Select options menu').getByText('PerfTest').click({ force: true });
+    await page.getByLabel('Select options menu').getByText('adx-test-db').click({ force: true });
     await page.getByText('KQL').click({ force: true });
     await page.waitForTimeout(10000);
     await page.getByTestId(selectors.components.queryEditor.codeEditor.container).click();
     await page.keyboard.press('Control+A');
-    await page.keyboard.insertText('PerfTest | where $__');
+    await page.keyboard.insertText('adxe2etest | where $__');
     await page.waitForTimeout(10000);
     await page.keyboard.press('Control+Space');
     await expect(page.getByText('$__timeFilter')).toBeVisible();
-    await page.keyboard.insertText('timeFilter(_Timestamp_) | order by _Timestamp_ asc');
+    await page.keyboard.insertText('timeFilter(timestamp) | order by timestamp asc');
     await page.keyboard.press('Shift+Enter');
     await page.waitForTimeout(6000);
-    expect(panel.panel.fieldNames).toContainText(['_val1_', '_val2_']);
+    expect(panel.panel.fieldNames).toContainText(['timestamp', 'name', 'index', 'jsonData', 'array', 'static']);
   });
 
   test('Create a builder query', async ({ dashboardPage, page, readProvisionedDataSource, grafanaVersion }) => {
@@ -112,10 +104,6 @@ test.describe('Azure Data Explorer queries', () => {
       fileName: 'adx.yaml',
     });
     const panel = await dashboardPage.addPanel();
-    await dashboardPage.timeRange.set({
-      from: '2017-09-22 12:00:00',
-      to: '2017-09-23 12:00:00',
-    });
     await panel.datasource.set(datasource.name);
     await panel.setVisualization('Table');
 
@@ -130,7 +118,7 @@ test.describe('Azure Data Explorer queries', () => {
       // data-testid was not passed to the select component prior to 11.1.0
       await panel.getQueryEditorRow('A').getByText('Cluster').click({ force: true });
     }
-    await page.getByLabel('Select options menu').getByText('grafanaadxdev').click({ force: true });
+    await page.getByLabel('Select options menu').getByText('adxtestclustere2e').click({ force: true });
     if (versionValue) {
       await panel
         .getQueryEditorRow('A')
@@ -140,15 +128,16 @@ test.describe('Azure Data Explorer queries', () => {
       // data-testid was not passed to the select component prior to 11.1.0
       await panel.getQueryEditorRow('A').getByText('Database').click({ force: true });
     }
-    await page.getByLabel('Select options menu').getByText('PerfTest').click({ force: true });
+    await page.getByLabel('Select options menu').getByText('adx-test-db').click({ force: true });
     await page
       .getByTestId(selectors.components.queryEditor.tableFrom.input)
       .getByLabel('Table', { exact: true })
       .click({ force: true });
-    await page.getByLabel('Select options menu').getByText('PerfTest').click({ force: true });
+    await page.getByLabel('Select options menu').getByText('adxe2etest').click({ force: true });
+    await page.waitForTimeout(6000);
 
     await page.getByTestId(selectors.components.queryEditor.runQuery.button).click();
     await page.waitForTimeout(6000);
-    expect(panel.panel.fieldNames).toContainText(['_val1_', '_val2_']);
+    expect(panel.panel.fieldNames).toContainText(['timestamp', 'name', 'index', 'jsonData', 'array', 'static']);
   });
 });
