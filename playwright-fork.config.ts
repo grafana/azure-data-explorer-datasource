d4b7940b4429b33cbb5cf9a882863507291f6139
Andreas Christou <andreas.christou@grafana.com>
2025-08-19 14:56:45 +0100

    Remove unused

--- playwright-fork.config.ts
+++ playwright-fork.config.ts
@@ -1,9 +1,6 @@
 import type { PluginOptions } from '@grafana/plugin-e2e';
 import { defineConfig } from '@playwright/test';
-import { dirname } from 'path';
 import playwrightConfig from './playwright.config';
-
-const pluginE2eAuth = `${dirname(require.resolve('@grafana/plugin-e2e'))}/auth`;
 
 export default defineConfig<PluginOptions>({
   ...playwrightConfig,
